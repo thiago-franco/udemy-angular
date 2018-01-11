a8f7ef8d3b74e3d833f83cae07ed8100a21d3848
Thiago Franco <thiagofranco.dev@gmail.com>
2018-01-10 23:57:33 -0200

    Creating components

---
+++ basics-assignment-1-start/src/app/dismiss-alert/dismiss.alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-dismiss-alert',
+    templateUrl: './dismiss.alert.component.html'
+})
+
+export class DismissAlertComponent {
+    
+}
